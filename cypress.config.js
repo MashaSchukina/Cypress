5aaafa3d9dab90f9fec0cf5a300417c718502bd6
Shcukina Mariia <mashaschukina49@gmail.com>
2026-06-28 02:21:03 +0300

    allure magic

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
     },
     baseUrl: 'https://api.clickup.com/api/v2',
     env: {
